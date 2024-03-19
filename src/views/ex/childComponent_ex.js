90ebcd4db35e85542595c747383c76c5f1187efb
khanh-b2014841 <khanh0969630781@gmail.com>
2024-03-19 23:23:49 +0700

    stateless/ stalefull

--- src/views/ex/childComponent_ex.js
+++ src/views/ex/childComponent_ex.js
@@ -1,20 +1,45 @@
 import React from "react";
 
 
-class ExChildComponent extends React.Component{
+// class ExChildComponent extends React.Component{
 
     
-    render() {
-        console.log(">>render", this.props)
-        // const name = this.props.name
-        // const age = this.props.age
+//     render() {
+//         console.log(">>render", this.props)
+//         // const name = this.props.name
+//         // const age = this.props.age
         
-        const {firstName,lastName, age, jobs} = this.props;
+//         const {firstName,lastName, age, jobs} = this.props;
 
-        // const name = "khanh"
+//         // const name = "khanh"
+//         return(
+//             <>
+//             <div>first name: {firstName} - lastName: {lastName} - age: {age}</div>
+//             <div className="job-list">
+//                 {
+//                     jobs.map((item, index)=>{
+//                         return(
+//                             <div key={item.id}>
+//                                 {item.title} - {item.salary}
+//                             </div>
+//                         )
+//                     })
+
+//                 }
+            
+//             </div>
+                
+//             </>
+//         )
+//     }
+// }
+
+const ExChildComponent = (props) =>{
+    console.log(">>>check child props", props)
+    const { jobs} = props;
         return(
             <>
-            <div>first name: {firstName} - lastName: {lastName} - age: {age}</div>
+            
             <div className="job-list">
                 {
                     jobs.map((item, index)=>{
@@ -31,7 +56,6 @@ class ExChildComponent extends React.Component{
                 
             </>
         )
-    }
 }
 
 export default ExChildComponent;
